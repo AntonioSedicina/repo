a9e1abd748986fd86d44dfbe9d0df489cc754671
AntonioSedicina <user24xyz@gmail.com>
2023-07-22 17:00:06 +0200

    date

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+        System.out.println(LocalDate.now());
         System.out.println("Hello it's me");
 
         // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
@@ -28,5 +29,25 @@ public class Main {
         System.out.println("Area of sq1: " + sq1.calculateArea());
         System.out.println("Area of sq2: " + sq2.calculateArea());
         System.out.println("Area of r1:  " + r1.calculateArea());
+        //using the Scanner
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String st1 = sc.nextLine();
+        System.out.println("string = " + st1);
+        // Get the current date
+        //LocalDate currentDate = LocalDate.now();
+        // Get the current year as an integer
+        //int year = currentDate.getYear();
+        //int yr = LocalDate.now().getYear();
+        //System.out.println("Current year: " + yr);
+        System.out.print("What year were you born? ");
+        int birth = sc.nextInt();
+        int age = LocalDate.now().getYear()-birth;
+        sc.nextLine();
+        System.out.println("You were born in "+birth+" so you're "+age);
+        System.out.println("Enter another string:");
+        String st2 = sc.nextLine();
+        System.out.println("string = " + st2);
+
     }
 }
